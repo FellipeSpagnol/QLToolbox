6d8ecdc444a23faea14db836c3452f17b1c7e6ec
FellipeSpagnol <fellipespagnoL@gmail.com>
2025-09-24 15:02:35 -0300

    class organization

---
+++ ql_core.py
@@ -0,0 +1,20 @@
+# Imports
+import numpy as np
+import math as m
+from typing import Optional, Union, List, Tuple, Dict
+
+
+# Class Definition
+class Agent:
+    def __init__(self) -> None:
+        pass
+
+
+class Environment:
+    def __init__(self) -> None:
+        pass
+
+
+# Training Function
+def train(agent: Agent, environment: Environment) -> None:
+    pass
